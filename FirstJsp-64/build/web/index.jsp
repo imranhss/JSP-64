4208afdc5a7a3a9ede89a542a51e333c8346a7b5
Muhammad Emran Hossain <emranhss@gmail.com>
2025-06-15 18:41:39 +0600

    Add

--- FirstJsp-64/build/web/index.jsp
+++ FirstJsp-64/build/web/index.jsp
@@ -14,48 +14,47 @@
 %>
 
 <div class="container">
-    
 
-    
-   <h1 class="text-primary text-center">All Student</h1> 
-   
-   <table class="table table-striped">
-       
-       <thead>
-           <tr>
-               <th>ID</th>
-               <th>Name</th>
-               <th>Email</th>
-               <th>Contact Number</th>
-               <th>Action</th>
-           </tr>
-       </thead>
-       
-       <tbody>
-           <c:forEach items="${list}" var="s">
-               <tr>
-                   <td>${s.getId()}</td>
-                   <td>${s.getName()}</td>
-                   <td>${s.getEmail()}</td>
-                   <td>${s.getContactNo()}</td>
-                   <td>
-                         <button type="submit" class="btn btn-primary">Edit</button>
-                        <button type="submit" class="btn btn-warning">Delete</button>
-                   </td>
-                   
-               </tr>
-               
-           </c:forEach>
-           
-       </tbody>
-       
-       
-   </table>
-   
+
+
+    <h1 class="text-primary text-center">All Student</h1> 
+
+    <table class="table table-striped">
+
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Contact Number</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <c:forEach items="${list}" var="s">
+                <tr>
+                    <td>${s.getId()}</td>
+                    <td>${s.getName()}</td>
+                    <td>${s.getEmail()}</td>
+                    <td>${s.getContactNo()}</td>
+                    <td>
+                        <a href="editstudentform.jsp?id=${s.id}" class="btn btn-primary">Edit</a>
+                        <a href="deletestudent.jsp?id=${s.id}" class="btn btn-danger" 
+                           onclick="return confirm('Are you sure you want to delete this student?');">Delete</a>
+                    </td>
+
+
+                </tr>
+
+            </c:forEach>
+
+        </tbody>
+
+
+    </table>
+
 </div>
-
-
-
 
 
 <%@include file="footer.jsp" %>
--- FirstJsp-64/src/java/dao/StudentDao.java
+++ FirstJsp-64/src/java/dao/StudentDao.java
@@ -51,6 +51,11 @@ public class StudentDao {
     }
 
     public static int saveStudent(Student s) {
+
+        if (!(s.getName().isEmpty())) {
+
+        }
+
         int status = 0;
         sql = "insert into student(name, email, contactNo, subject, gender) values(?,?,?,?,?)";
 
@@ -73,6 +78,81 @@ public class StudentDao {
             Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+        return status;
+    }
+
+    public static void deleteStudent(int id) {
+        sql = "delete from student where id= ?";
+
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ps.executeUpdate();
+
+            ps.close();
+            DbUtil.getCon().close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static Student getById(int id) {
+        Student s = null;
+        sql = "select * from student where id=?";
+
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            ps.setInt(1, id);
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                s = new Student(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("contactNo"),
+                        rs.getString("subject"),
+                        rs.getString("gender")
+                );
+            }
+
+            rs.close();
+            ps.close();
+            DbUtil.getCon().close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return s;
+    }
+
+    public static int updateStudent(Student s) {
+        int status = 0;
+        sql = "update student set name= ?, email= ?, contactNo= ?, gender=?, subject=? where id=?";
+
+        try {
+            ps = DbUtil.getCon().prepareStatement(sql);
+            ps.setString(1, s.getName());
+            ps.setString(2, s.getEmail());
+            ps.setString(3, s.getContactNo());
+            ps.setString(4, s.getGender());
+            ps.setString(5, s.getSubject());
+            ps.setInt(6, s.getId());
+
+            status = ps.executeUpdate();
+
+            System.out.println(status);
+
+            ps.close();
+            DbUtil.getCon().close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return status;
     }
 
